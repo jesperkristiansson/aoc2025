76a80ac8dd18a03425a30a9e9649fdc4b0e11cb0
Jesper Kristiansson <jesper.g.kristiansson@gmail.com>
2025-12-04 10:14:35 +0100

    day 4: part 1

---
+++ day4/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const paperRune = '@'
+const maxAdjacentPapers = 3
+
+func getGrid() [][]bool {
+	var grid [][]bool
+
+	var scanner = bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		gridLine := make([]bool, 0, len(line))
+		for _, r := range line {
+			gridLine = append(gridLine, r == paperRune)
+		}
+
+		grid = append(grid, gridLine)
+	}
+
+	return grid
+}
+
+// Assumes that the grid is not empty and that each row is the same length
+func isInsideGrid(grid [][]bool, r, c int) bool {
+	return r >= 0 && r < len(grid) && c >= 0 && c < len(grid[0])
+}
+
+func isPaper(grid [][]bool, r, c int) bool {
+	if !isInsideGrid(grid, r, c) {
+		return false
+	}
+	return grid[r][c]
+}
+
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func countAdjacentPapers(grid [][]bool, r, c int) int {
+	papers := 0
+
+	papers += boolToInt(isPaper(grid, r-1, c-1))
+	papers += boolToInt(isPaper(grid, r-1, c))
+	papers += boolToInt(isPaper(grid, r-1, c+1))
+	papers += boolToInt(isPaper(grid, r, c-1))
+	papers += boolToInt(isPaper(grid, r, c+1))
+	papers += boolToInt(isPaper(grid, r+1, c-1))
+	papers += boolToInt(isPaper(grid, r+1, c))
+	papers += boolToInt(isPaper(grid, r+1, c+1))
+
+	return papers
+}
+
+func part1(grid [][]bool) int {
+	result := 0
+	for r := range grid {
+		for c := range grid[r] {
+			if isPaper(grid, r, c) && countAdjacentPapers(grid, r, c) <= maxAdjacentPapers {
+				result++
+			}
+		}
+	}
+	return result
+}
+
+func part2(ranges [][]bool) int {
+	result := 0
+	return result
+}
+
+func main() {
+	grid := getGrid()
+	fmt.Println("part 1:", part1(grid))
+	fmt.Println("part 2:", part2(grid))
+}
