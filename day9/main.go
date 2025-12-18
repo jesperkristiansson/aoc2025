d2144a1d70e08feb62372395c389561dcf44139f
Jesper Kristiansson <jesper.g.kristiansson@gmail.com>
2025-12-18 21:55:51 +0100

    day 9: part 2

--- day9/main.go
+++ day9/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"os"
+	"slices"
 )
 
 type point2d struct {
@@ -63,10 +65,111 @@ func part1(input input) int {
 	return max
 }
 
-func part2(input input) int {
+func det(p1, p2 point2d) int {
+	return p1.x*p2.y - p2.x*p1.y
+}
+
+func orientation(points input) int {
 	sum := 0
+	for i := 0; i < len(points)-1; i++ {
+		sum += det(points[i], points[i+1])
+	}
+	sum += det(points[len(points)-1], points[0])
+
+	if sum == 0 {
+		panic("Ill-formed polygon")
+	}
 
 	return sum
+}
+
+type xLine struct {
+	x            int
+	startY, endY int
+}
+
+type yLine struct {
+	y            int
+	startX, endX int
+}
+
+func getLines(points input) ([]xLine, []yLine) {
+	var xLines []xLine
+	var yLines []yLine
+
+	for i := 0; i < len(points); i++ {
+		nextI := i + 1
+		if nextI == len(points) {
+			nextI = 0
+		}
+		p1 := points[i]
+		p2 := points[nextI]
+		switch {
+		case p1.x == p2.x:
+			lowY := min(p1.y, p2.y)
+			highY := max(p1.y, p2.y)
+			line := xLine{x: p1.x, startY: lowY, endY: highY}
+			xLines = append(xLines, line)
+		case p1.y == p2.y:
+			lowX := min(p1.x, p2.x)
+			highX := max(p1.x, p2.x)
+			line := yLine{y: p1.y, startX: lowX, endX: highX}
+			yLines = append(yLines, line)
+		default:
+			panic("points do not share x- or y-coordinates")
+		}
+	}
+
+	return xLines, yLines
+}
+
+func part2(points input) int {
+	// Not needed for the input data
+	orientation := orientation(points)
+	isRight := orientation > 0
+	_ = isRight
+
+	xLines, yLines := getLines(points)
+
+	slices.SortFunc(xLines, func(a, b xLine) int { return cmp.Compare(a.x, b.x) })
+	slices.SortFunc(yLines, func(a, b yLine) int { return cmp.Compare(a.y, b.y) })
+
+	maxArea := 0
+
+	for i, p1 := range points {
+	loop:
+		for _, p2 := range points[i+1:] {
+			// TODO: check orientation
+
+			topLeft := point2d{x: min(p1.x, p2.x), y: min(p1.y, p2.y)}
+			bottomRight := point2d{x: max(p1.x, p2.x), y: max(p1.y, p2.y)}
+
+			lowXIdx, _ := slices.BinarySearchFunc(xLines, topLeft.x+1, func(a xLine, x int) int { return cmp.Compare(a.x, x) })
+			highXIdx, _ := slices.BinarySearchFunc(xLines, bottomRight.x, func(a xLine, x int) int { return cmp.Compare(a.x, x) })
+			for xIdx := lowXIdx; xIdx < highXIdx; xIdx++ {
+				line := xLines[xIdx]
+				if !(line.startY >= bottomRight.y || line.endY <= topLeft.y) {
+					continue loop
+				}
+			}
+
+			lowYIdx, _ := slices.BinarySearchFunc(yLines, topLeft.y+1, func(a yLine, y int) int { return cmp.Compare(a.y, y) })
+			highYIdx, _ := slices.BinarySearchFunc(yLines, bottomRight.y, func(a yLine, y int) int { return cmp.Compare(a.y, y) })
+			for yIdx := lowYIdx; yIdx < highYIdx; yIdx++ {
+				line := yLines[yIdx]
+				if !(line.startX >= bottomRight.x || line.endX <= topLeft.x) {
+					continue loop
+				}
+			}
+
+			area := rectangleArea(p1, p2)
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+	}
+
+	return maxArea
 }
 
 func main() {
